ba62994cd512d4bf597d94387bb9a8fb27c40541
Vladimir <vgorodnyuk@gmail.com>
2014-10-02 18:42:54 +0300

    Update GuessTheNum.cpp

--- GuessTheNum.cpp
+++ GuessTheNum.cpp
@@ -4,27 +4,32 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	srand(time(NULL));
-	WCHAR* str=new wchar_t[200];
+	WCHAR* str = new wchar_t[200];
 	int max = 500;
 	int min = 0;
 	int num = rand() % max;
+	int count = 1;
 	wsprintf(str, TEXT("%d"), num);
 	int A = MessageBox(NULL, str, TEXT("Попробую угадать число от 0 до 500"), MB_YESNOCANCEL | MB_ICONINFORMATION);
 	do{
 		if (A == IDYES)
 		{
-			max = num-1;
-			num = rand() % (max - min)+min;			
+			max = num - 1;
+			num = rand() % (max - min) + min;
 			wsprintf(str, TEXT("%d"), num);
 			A = MessageBox(NULL, str, TEXT("Попробую угадать число от 0 до 500"), MB_YESNOCANCEL | MB_ICONINFORMATION);
 		}
 		if (A == IDNO)
 		{
-			min = num+1;
+			min = num + 1;
 			num = rand() % (max - min) + min;
 			wsprintf(str, TEXT("%d"), num);
 			A = MessageBox(NULL, str, TEXT("Попробую угадать число от 0 до 500"), MB_YESNOCANCEL | MB_ICONINFORMATION);
 		}
+		count++;
 	} while (A != IDCANCEL);
+	wsprintf(str, TEXT("Число угадано за %d попыток"), count);
+	MessageBox(NULL, str, TEXT("Число угадано за ..."), MB_OK | MB_ICONINFORMATION);
 	return 0;
 }
+
